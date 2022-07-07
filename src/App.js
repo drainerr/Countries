33859602acf4df83ea7e6b5cc080a08c817ac33b
drainerr <george.kvrivi@gmail.com>
2022-07-07 14:49:12 +0400

    add 404.html

--- src/App.js
+++ src/App.js
@@ -3,33 +3,38 @@ import GlobalStyle from './styles/Global';
 import Find from './Components/Search-Filter/Find';
 import React, { useContext, Fragment } from 'react';
 import Countries from './Components/Countries/CountriesWrapper';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 import Details from './Components/Details/Details';
 import { CountriesCtx } from './hooks/useCtx';
 
 function App() {
-  const { dark } = useContext(CountriesCtx);
-  return (
-    <Fragment>
-      <Router>
-        <GlobalStyle dark={dark ? 'dark' : 'light'} />
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Find />
-                <Countries />
-              </>
-            }
-          />
-          <Route path="/countries/:name" element={<Details />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
-    </Fragment>
-  );
+	const { dark } = useContext(CountriesCtx);
+	return (
+		<Fragment>
+			<Router basename='/Countries'>
+				<GlobalStyle dark={dark ? 'dark' : 'light'} />
+				<Header />
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<>
+								<Find />
+								<Countries />
+							</>
+						}
+					/>
+					<Route path='/countries/:name' element={<Details />} />
+					<Route path='*' element={<Navigate to='/' />} />
+				</Routes>
+			</Router>
+		</Fragment>
+	);
 }
 
 export default App;
